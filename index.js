2a689fc3eca56991015bc5062a9a50823e33b4b4
23h1 <nair.abhijeet@gmail.com>
2022-10-12 17:28:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+function App(){
+    const [todos, setTodos] = React.useState([
+        {
+            text: 'learn React',
+            isCompleted: false,
+        },
+        {
+            text: 'meet friend for lunch',
+            isCompleted: false,
+        },
+        {
+            text: 'build todo app',
+            isCompleted: false,
+        }
+    ]);
+    const [value, setValue] = React.useState('');
+
+    const handleSubmit = e=> {
+        //e.preventDefault();
+        if (!value) return;
+        const newTodos = [...todos, {text:value, isCompleted:false}];
+        setTodos(newTodos);
+        setValue('');
+    }
+
+    return (<>
+        {todos.map((todo,i) => 
+            <div className="todo" key={i}>{todo.text}</div>)} 
+            
+            <form onSubmit={handleSubmit}>
+                <input
+                    type = "text"
+                    className="input"
+                    value={value}
+                    placeholder="Add To list"
+                    onChange={e => setValue(e.target.value)}
+                />
+            </form>
+            
+            </>);
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
